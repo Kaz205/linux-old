69bbaf224385e8550e96569707773c57fcb60ce6
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-18 03:39:20 +0900

    zsmalloc: add bp hints to memory allocations
    These are called frequently.
    
    Add bp hints to avoid pipeline flushes.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1106,7 +1106,7 @@ static struct zspage *alloc_zspage(struct zs_pool *pool,
 	struct page *pages[ZS_MAX_PAGES_PER_ZSPAGE];
 	struct zspage *zspage = cache_alloc_zspage(pool, gfp);
 
-	if (!zspage)
+	if (unlikely(!zspage))
 		return NULL;
 
 	zspage->magic = ZSPAGE_MAGIC;
@@ -1484,7 +1484,7 @@ unsigned long zs_malloc(struct zs_pool *pool, size_t size, gfp_t gfp)
 		return (unsigned long)ERR_PTR(-EINVAL);
 
 	handle = cache_alloc_handle(pool, gfp);
-	if (!handle)
+	if (unlikely(!handle))
 		return (unsigned long)ERR_PTR(-ENOMEM);
 
 	/* extra space in chunk to keep the handle */
@@ -1507,7 +1507,7 @@ unsigned long zs_malloc(struct zs_pool *pool, size_t size, gfp_t gfp)
 	spin_unlock(&pool->lock);
 
 	zspage = alloc_zspage(pool, class, gfp);
-	if (!zspage) {
+	if (unlikely(!zspage)) {
 		cache_free_handle(pool, handle);
 		return (unsigned long)ERR_PTR(-ENOMEM);
 	}
