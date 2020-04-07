2adfeec689a09244d2fd5904c3bba8161abe4c4b
Jason A. Donenfeld <Jason@zx2c4.com>
2020-04-06 23:38:15 -0600

    x86/mce/therm_throt: remove unused platform_thermal_notify function pointer
    A long time ago platform_thermal_notify was added as some generic
    mechanism for platform drivers to hook thermal events. It seems as
    though this has been entirely superseded, and nothing uses it. Remove
    the plumbing for this, since this code runs in an interrupt hot path.
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- drivers/thermal/intel/thermal_interrupt.h
+++ drivers/thermal/intel/thermal_interrupt.h
@@ -5,9 +5,6 @@
 /* Interrupt Handler for package thermal thresholds */
 extern int (*platform_thermal_package_notify)(__u64 msr_val);
 
-/* Interrupt Handler for core thermal thresholds */
-extern int (*platform_thermal_notify)(__u64 msr_val);
-
 /* Callback support of rate control, return true, if
  * callback has rate control */
 extern bool (*platform_thermal_package_rate_control)(void);
