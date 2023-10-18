51aaddecd5d72925064ee6a1d95bab0c7dd3b6d2
Kazuki H <kazukih0205@gmail.com>
2023-10-18 22:12:17 +0900

    px4_drv: Add inline compilation setup

---
+++ drivers/media/usb/px4_drv/driver/revision.h
@@ -0,0 +1,10 @@
+// revision.h
+
+#ifndef __REVISION_H__
+#define __REVISION_H__
+
+#define REVISION_NUMBER	"384"
+#define REVISION_NAME	"develop"
+#define COMMIT_HASH	"7fa9f05d2cbdf1d821f479248d561f9868051b8b"
+
+#endif
