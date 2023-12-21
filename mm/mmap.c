097f50c189097d6f3a33d3b8f61d31082d4e3a13
Yang Shi <yang@os.amperecomputing.com>
2023-12-20 22:59:42 -0800

    mm: mmap: no need to call khugepaged_enter_vma() for stack
    We avoid allocating THP for temporary stack, even tnough
    khugepaged_enter_vma() is called for stack VMAs, it actualy returns false.
    So no need to call it in the first place at all.
    
    Link: https://lkml.kernel.org/r/20231221065943.2803551-1-shy828301@gmail.com
    Signed-off-by: Yang Shi <yang@os.amperecomputing.com>
    Reviewed-by: Yin Fengwei <fengwei.yin@intel.com>
    Cc: Christopher Lameter <cl@linux.com>
    Cc: "Huang, Ying" <ying.huang@intel.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: kernel test robot <oliver.sang@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2056,7 +2056,6 @@ static int expand_upwards(struct vm_area_struct *vma, unsigned long address)
 		}
 	}
 	anon_vma_unlock_write(vma->anon_vma);
-	khugepaged_enter_vma(vma, vma->vm_flags);
 	mas_destroy(&mas);
 	validate_mm(mm);
 	return error;
@@ -2150,7 +2149,6 @@ int expand_downwards(struct vm_area_struct *vma, unsigned long address)
 		}
 	}
 	anon_vma_unlock_write(vma->anon_vma);
-	khugepaged_enter_vma(vma, vma->vm_flags);
 	mas_destroy(&mas);
 	validate_mm(mm);
 	return error;
