79c89e2bdab35a3d59f98f04d2afccd3ad93331f
Kazuki H <kazukih0205@gmail.com>
2024-06-20 19:58:46 +0900

    Revert "tcp/bbr3: initial import"
    This reverts commit 2308a069fa84f02361b1c77eef48fb0e30cf1ef8.

--- net/ipv4/bpf_tcp_ca.c
+++ net/ipv4/bpf_tcp_ca.c
@@ -302,13 +302,9 @@ static void bpf_tcp_ca_pkts_acked(struct sock *sk, const struct ack_sample *samp
 {
 }
 
-static u32 bpf_tcp_ca_tso_segs(struct sock *sk, unsigned int mss_now)
+static u32 bpf_tcp_ca_min_tso_segs(struct sock *sk)
 {
 	return 0;
-}
-
-static void bpf_tcp_ca_skb_marked_lost(struct sock *sk, const struct sk_buff *skb)
-{
 }
 
 static void bpf_tcp_ca_cong_control(struct sock *sk, const struct rate_sample *rs)
@@ -340,8 +336,7 @@ static struct tcp_congestion_ops __bpf_ops_tcp_congestion_ops = {
 	.cwnd_event = bpf_tcp_ca_cwnd_event,
 	.in_ack_event = bpf_tcp_ca_in_ack_event,
 	.pkts_acked = bpf_tcp_ca_pkts_acked,
-	.tso_segs = bpf_tcp_ca_tso_segs,
-	.skb_marked_lost = bpf_tcp_ca_skb_marked_lost,
+	.min_tso_segs = bpf_tcp_ca_min_tso_segs,
 	.cong_control = bpf_tcp_ca_cong_control,
 	.undo_cwnd = bpf_tcp_ca_undo_cwnd,
 	.sndbuf_expand = bpf_tcp_ca_sndbuf_expand,
